e014b73232590cc8aeb518cd7ddcd1a8c9c9ca0d
王昱蘅 <sherrikevin5143@gmail.com>
2023-01-09 19:44:43 +0800

    build project structure

---
+++ Client/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.typer.telegrapher {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.typer.telegrapher to javafx.fxml;
+    exports com.typer.telegrapher;
+}
