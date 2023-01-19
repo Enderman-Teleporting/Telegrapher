6265df52667d849996a51d41951e67215e380b78
王昱蘅 <sherrikevin5143@gmail.com>
2023-01-19 18:37:07 +0800

    window and kt update

--- Client/src/main/java/com/typer/telegrapher/Main.java
+++ Client/src/main/java/com/typer/telegrapher/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),550,600);
+        Scene scene = new Scene(fxmlLoader.load(),560,650);
         stage.setResizable(false);
         stage.setTitle("Telegrapher");
         stage.setScene(scene);
---
+++ Client/src/main/java/com/typer/telegrapher/Windows/EventHandler.kt
@@ -0,0 +1,59 @@
+package com.typer.telegrapher.Windows
+
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+
+class EventHandler {
+    @FXML
+    private val msg: Button? = null
+
+    @FXML
+    private val exit: Button? = null
+
+    @FXML
+    private val txtMsg: Label? = null
+
+    @FXML
+    private val Port: TextField? = null
+
+    @FXML
+    private val Connect: Button? = null
+
+    @FXML
+    private val RepoLoc: Button? = null
+
+    @FXML
+    private val License: Button? = null
+
+    @FXML
+    private val AuthorInfo: Button? = null
+
+    @FXML
+    private val txtConnect: Label? = null
+    @FXML
+    fun onConnectClick(event: ActionEvent?) {
+    }
+
+    @FXML
+    fun onMsgClick(event: ActionEvent?) {
+    }
+
+    @FXML
+    fun onExitClick(event: ActionEvent?) {
+    }
+
+    @FXML
+    fun onAuthorInfoClick(event: ActionEvent?) {
+    }
+
+    @FXML
+    fun onRepoLocationClick(event: ActionEvent?) {
+    }
+
+    @FXML
+    fun onLicenseClick(event: ActionEvent?) {
+    }
+}
